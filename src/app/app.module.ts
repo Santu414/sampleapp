c6bce094ed1c4e3b67ffc05c2469af727ab3db0e
santu414 <snagankera@gmail.com>
2017-12-15 06:14:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { AllComponent } from './all/all.component';
+import { MenComponent } from './men/men.component';
+import { WomenComponent } from './women/women.component';
+import { KidsComponent } from './kids/kids.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { SportsComponent } from './sports/sports.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllComponent,
+    MenComponent,
+    WomenComponent,
+    KidsComponent,
+    ElectronicsComponent,
+    SportsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'all',component:AllComponent},
+      {path:'men',component:MenComponent},
+      {path:'women',component:WomenComponent},
+      {path:'kids',component:KidsComponent},
+      {path:'electronics',component:ElectronicsComponent},
+      {path:'sports',component:SportsComponent},
+      {path:"",redirectTo:'all',pathMatch:'full'},
+      {path:"**",redirectTo:'all',pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
